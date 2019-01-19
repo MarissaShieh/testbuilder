f41ef2d88da3e288c3124770b459ee237f7a77e0
Marissa <marissa.shieh@gmail.com>
2019-01-18 22:20:21 -0500

    Checks input is a cardnumber
    Makes sure that the "cardNumber" is indeed a valid card number before trying to determine which network it belongs to.

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,6 +13,16 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+//Check if all numbers and right number of numbers
+if (isNaN(cardNumber)){
+  return "ERROR: Not a number."
+} else if (cardNumber.toString().length > 14 || cardNumber.toString().length < 15) {
+  return "ERROR: Not a valid number"
+}
+
+
+
 };
 
 
